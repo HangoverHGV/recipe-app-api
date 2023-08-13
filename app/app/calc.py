e7600fe0641397bac2dea299e0cc2eec53dffcf1
Hango Bogdan Ioan <hangobogdan@gmail.com>
2023-08-13 13:09:25 +0300

    commit

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+'''
+Calculate functions
+'''
+
+def add(x, y):
+
+    '''
+    Add x and y
+    :return: result
+    '''
+
+    return x + y
+
+def subtract (x, y):
+    ''' Subtract x from y and return result '''
+
+    return y - x
