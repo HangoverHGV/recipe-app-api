5a6ac5af3a6cf8bbb9096b98359d9cf7c31be3be
Hango Bogdan Ioan <hangobogdan@gmail.com>
2023-08-31 11:35:13 +0300

    Test User Token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -83,7 +83,7 @@ class PublicUserApiTests(TestCase):
         create_user(email='test@example.com', password='goodpass')
 
         payload = {'email': 'test@example.com', 'password': 'badpass'}
-        res = self.client.post((TOKEN_URL, payload))
+        res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -93,4 +93,5 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertNotIn(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
