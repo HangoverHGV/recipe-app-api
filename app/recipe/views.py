b68ecb2670c2affe67ea0e80e8c8e704e0a8c1bc
Hango Bogdan Ioan <hangobogdan@gmail.com>
2023-10-02 14:55:19 +0300

    create Nested Serializers

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,7 +35,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     '''Manage tags in the database'''
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -45,5 +48,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         '''Filter querryset to authenticated user'''
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
-
