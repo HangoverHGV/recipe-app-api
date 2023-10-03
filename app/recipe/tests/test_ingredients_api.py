5056cdaffbc2d7b35b8fcb3ee86e815e29dc0bcd
Hango Bogdan Ioan <hangobogdan@gmail.com>
2023-10-03 13:02:55 +0300

    Ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -16,8 +16,8 @@ from recipe.serializers import IngredientSerializer
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 def detail_url(ingredient_id):
-    '''Create and return an ingredientd detail URL.'''
-    return reverse('recipe:detail', args=[ingredient_id])
+    '''Create and return an ingredient detail URL.'''
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
 
 def create_user(email='user@exampl.com', password='testpass123'):
     '''Create and return user'''
@@ -80,3 +80,13 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
+
+    def test_delete_ingredient(self):
+        '''Test deleting an ingredients'''
+        ingredient = Ingredient.objects.create(user=self.user, name='Lettuce')
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Ingredient.objects.filter(user=self.user).exists())
